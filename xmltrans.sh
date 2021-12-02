b757ea5c651c28a314dd4c69d32cc7f61243fbda
james602152002@hotmail.com <james602152002@gmail.com>
2021-12-02 15:36:07 +0800

    update

--- xmltrans.sh
+++ xmltrans.sh
@@ -6,7 +6,7 @@ unzip ~/Documents/localization/localization.zip -d  ~/Documents/localization/
 ./mergeLoc.py
 rm ~/Downloads/localization.zip
 cd ~/Documents/localization_merged
-zip -r localization.zip Saury* localization_merged
+zip -r localization.zip Saury*
 cd ~/scripts/XMLTrans
 cp ~/Documents/localization_merged/localization.zip ~/Downloads
 rm -r ~/Documents/localization_merged
